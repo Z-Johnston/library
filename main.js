553f2897cbdb5896953d7773aedd429c34cd6e5b
Zack Johnston <zackjohnston@me.com>
2022-07-26 17:35:19 -0500

    structure added to webpage

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const addBookButton = document.querySelector('.add-book');
+
+let library = [];
+
+//
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,6 +10,13 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not read yet' : 'has been read'}`;
 }
+
+//
+function addBookToLibrary(title, author, pages, read) { 
+    return library.push(new Book(title, author, pages, read))
+};
+
