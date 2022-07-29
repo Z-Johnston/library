010bc21583da7c13b9bc78f0d22203f9d4a96604
Zack Johnston <zackjohnston@me.com>
2022-07-28 19:11:43 -0500

    functionality added to post a single book card to library

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const addBookButton = document.querySelector('.add-book');
+const submitButton = document.querySelector('.submit-button');
+const bookInput = document.querySelectorAll('input');
+const library = document.querySelector('.library');
 
-let library = [];
-
-//
+//Book object constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -11,12 +11,54 @@ function Book(title, author, pages, read) {
 }
 
 //
+let libraryArray = [];
+
+//Info function to return full Book info 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${!this.read ? 'not read yet' : 'has been read'}`;
 }
 
-//
-function addBookToLibrary(title, author, pages, read) { 
-    return library.push(new Book(title, author, pages, read))
+// Add book function to create a new Book object from the submitted
+// form data and add that Book to the library array
+function addBookInputToLibrary() { 
+    const book = new Book(
+        bookInput[0].value,
+        bookInput[1].value,
+        bookInput[2].value,
+        bookInput[3].checked);
+        libraryArray.push(book);
 };
 
+//
+function displayLibrary() {
+    for (let i = 0; i < libraryArray.length; i++) {
+        const book = document.createElement('div');
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPages = document.createElement('div');
+        const bookRead = document.createElement('div');
+
+        book.classList.add('book');
+        bookTitle.classList.add('book-title');
+        bookAuthor.classList.add('book-author');
+        bookPages.classList.add('book-pages');
+        bookRead.classList.add('book-read');
+        
+        bookTitle.textContent = libraryArray[i].title;
+        bookAuthor.textContent = libraryArray[i].author;
+        bookPages.textContent = libraryArray[i].pages;
+        bookRead.textContent = libraryArray[i].read;
+        
+        book.appendChild(bookTitle);
+        book.appendChild(bookAuthor);
+        book.appendChild(bookPages);
+        book.appendChild(bookRead);
+        library.appendChild(book);
+    } 
+}
+
+//Event listener to create + add book to library and show the updated library
+submitButton.addEventListener('click', function() {
+    addBookInputToLibrary();
+    displayLibrary();
+});
